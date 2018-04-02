d3c438bfc18f4af5fd6b2989769281ea3aa831ec
yanl <yanl@yxt.com>
2018-04-02 18:18:21 +0800

    up

--- webDemo/src/openDemo/service/sync/jrtt/JrttConfig.java
+++ webDemo/src/openDemo/service/sync/jrtt/JrttConfig.java
@@ -1,9 +1,9 @@
 package openDemo.service.sync.jrtt;
 
 public interface JrttConfig {
-	String apikey = "11f62229-70f6-41c0-8675-1334ad32e9a0";
+	String apikey = "";
 	
-	String secretkey = "d2331646-0d1d-4348-ae65-c126b8496538";
+	String secretkey = "";
 
 	String baseUrl = "http://api.qida.yunxuetang.com.cn/";
 
