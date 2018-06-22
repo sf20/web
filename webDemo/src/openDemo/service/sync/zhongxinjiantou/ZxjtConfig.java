db263a80a4afb862dd0eda960ec04cf8ca304983
Administrator <Administrator@yanliang>
2018-06-22 10:29:41 +0800

    add key

--- webDemo/src/openDemo/service/sync/zhongxinjiantou/ZxjtConfig.java
+++ webDemo/src/openDemo/service/sync/zhongxinjiantou/ZxjtConfig.java
@@ -1,10 +1,10 @@
 package openDemo.service.sync.zhongxinjiantou;
 
 public interface ZxjtConfig {
-	String apikey = "";
+	String apikey = "21d0b041-13d4-4670-a107-626443693182";
 
-	String secretkey = "";
+	String secretkey = "552333e8-36b7-4eb1-a131-4aa63004e92b";
 
-	String baseUrl = "";
+	String baseUrl = "http://api.qida.yunxuetang.com.cn/";
 
 }
