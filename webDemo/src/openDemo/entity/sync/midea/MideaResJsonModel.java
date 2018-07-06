1a02b59950405dc43698ef045adf9c05f8879f88
Administrator <Administrator@yanliang>
2018-07-06 15:25:49 +0800

    JsonModel修改

--- webDemo/src/openDemo/entity/sync/midea/MideaResJsonModel.java
+++ webDemo/src/openDemo/entity/sync/midea/MideaResJsonModel.java
@@ -1,10 +1,7 @@
 package openDemo.entity.sync.midea;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * 美的物业接口返回的json数据模型
@@ -15,11 +12,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  *            body类型：员工类型或组织类型
  */
 public class MideaResJsonModel<T> {
-	@JsonProperty("head")
-	Map<String, String> head = new HashMap<>();
-
-	@JsonProperty("body")
-	List<T> body;
+	private Map<String, String> head;
+	private List<T> body;
 
 	public Map<String, String> getHead() {
 		return head;
